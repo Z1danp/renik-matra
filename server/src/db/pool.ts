5a36b80dd6f0ed56a06d488ef66bdf084ebb5b1a
StarHub190 <muhammadzidanpriadi843@gmail.com>
2026-08-03 19:56:24 +0700

    fix setup prisma

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -2,11 +2,11 @@ import { PrismaClient } from "@prisma/client/extension";
 import { Pool, neon } from "@neondatabase/serverless";
 
 export const prisma = new PrismaClient({
-  log: ["error", "warn"],
+  datasourceUrl: process.env.DATABASE_URL,
 });
 
 export const rawPool = new Pool({
-    connectionString: process.env.DATABASE_URL
-})
+  connectionString: process.env.DATABASE_URL,
+});
 
-export const sql = neon(process.env.DATABASE_URL!)
+export const sql = neon(process.env.DATABASE_URL!);
